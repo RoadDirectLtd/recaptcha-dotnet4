d7b9342ae317e3aee6e83eca3eb94b7a587106e2
Daniel Stocker <development@roaddirect.co.nz>
2024-03-21 22:08:49 +1300

    convert to SDK style project
    Using .NET Upgrade Assistant VS extension

--- src/Recaptcha.Web/Properties/AssemblyInfo.cs
+++ src/Recaptcha.Web/Properties/AssemblyInfo.cs
@@ -6,18 +6,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Road Direct reCAPTCHA.NET")]
-[assembly: AssemblyDescription("A library that allows a developer to easily integrate Google's free Recaptcha service with an ASP.NET Web Forms or ASP.NET MVC Web application. Forked from tanveery.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Road Direct")]
-[assembly: AssemblyProduct("reCAPTCHA for .NET 4")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -25,16 +13,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f46554d9-236b-459d-bec0-bc3249aeea98")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
